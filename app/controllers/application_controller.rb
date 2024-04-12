a5f7cd804a72d248689cb3302ff8824d76bda6f7
Paul Kim <pkim@hey.com>
2024-04-11 21:06:48 -0400

    Add helper methods to check if user is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    Current.user ||= authenticate_user_from_session
+  end
+  helper_method :current_user
+
+  def authenticate_user_from_session
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      user if user.present?
+    end
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
+
   def sign_in(user)
     Current.user = user
     reset_session
