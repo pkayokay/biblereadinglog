29d357e26a5866827b5d5f3db81f8d47cb550990
Paul Kim <pkim@hey.com>
2024-04-13 15:21:47 -0400

    Update toggle chapter to destroy or create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,21 +9,13 @@ class BooksController < ApplicationController
     book = Book.find(params[:book_id])
     chapter_number = params[:chapter_number].to_i
 
-    chapters_data = book.chapters_data
-    chapter_to_update = chapters_data.find { |chapter| chapter["chapter_number"] == chapter_number}
+    chapter = book.chapters.find_by(chapter_number: chapter_number)
+    toggle = chapter.present? ? chapter.destroy : book.chapters.create(chapter_number: chapter_number)
 
-    if chapter_to_update
-      if chapter_to_update["completed_at"].present?
-        chapter_to_update["completed_at"] = nil
-      else
-        chapter_to_update["completed_at"] = Time.zone.now
-      end
-    end
-
-    if book.update(chapters_data: chapters_data)
+    if toggle
       respond_to do |format|
         format.turbo_stream { render turbo_stream: [
-          turbo_stream.replace("book-#{book.id}-#{chapter_number}", partial: "books/book_chapter_form", locals: { book: book, chapter_data: chapter_to_update}),
+          turbo_stream.replace("book-#{book.id}-#{chapter_number}", partial: "books/book_chapter_form", locals: { book: book, chapter_number: chapter_number}),
           turbo_stream.update("book-header-#{book.id}", partial: "books/book_chapter_header", locals: { book: book, show_page: true })
           ]
         }
