3b2f808257b21b49e38b286805316ff37f93e5aa
Paul Kim <pkim@hey.com>
2024-04-12 20:00:00 -0400

    Generate table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_004609) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_12_234443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reading_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reading_logs_on_user_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_12_004609) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "reading_logs", "users"
 end
