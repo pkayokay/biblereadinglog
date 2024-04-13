fab3f9856f9beb5f7c9cedbc4a7550febdd5ee8c
Paul Kim <pkim@hey.com>
2024-04-13 14:59:55 -0400

    Remove chapter creation

--- app/services/create_books_service.rb
+++ app/services/create_books_service.rb
@@ -13,15 +13,7 @@ class CreateBooksService
           reading_log: @reading_log,
           position: book["position"],
           chapters_count: book["chapters_count"],
-          chapters_data: book["chapters_count"].times.map do |index|
-            chapter = index + 1
-            { chapter_number: chapter, completed_at: nil }
-          end
         )
-
-        book["chapter_count"].times.map do |index|
-          Chapter.create!(chapter_number: index + 1, book: book_record)
-        end
       end
     rescue => e
       return { error: e.record.errors.full_messages.first }
