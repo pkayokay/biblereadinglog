32c2ec517bf7c9771ad19a003e489d9a0e7a1adf
Paul Kim <pkim@hey.com>
2024-04-12 21:05:49 -0400

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   normalizes :email, with: ->(email) { email.strip.downcase }
   has_one :reading_log, dependent: :destroy
 end
