f51b8fb73f9e06192c4b5b87c32a66d08c5c0376
Paul Kim <pkim@hey.com>
2024-04-13 10:57:54 -0400

    Add service to create books from json file

--- app/services/setup_user_service.rb
+++ app/services/setup_user_service.rb
@@ -6,16 +6,20 @@ class SetupUserService
 
   def call
     User.transaction do
-      user = User.create!(email: @email, password: @password)
-      ReadingLog.create!(user: user)
+      @user = User.create!(email: @email, password: @password)
+      @reading_log = ReadingLog.create!(user: @user)
+      @books = CreateBooksService.new(reading_log: @reading_log).call
     rescue => e
-      return { error: e.message }
+      return { error: e.record.errors.full_messages.first }
     end
 
     {
+      data: {
+        user: @user,
+        reading_log: @reading_log,
+        books: @books[:data]
+      },
       success: true,
-      email: @email,
-      password: @password,
     }
   end
 end
