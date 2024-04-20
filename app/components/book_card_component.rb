49fb6e6130e5821227430608e1fc81069b8ee16f
Paul Kim <pkim@hey.com>
2024-04-20 14:09:05 -0400

    Update

---
+++ app/components/book_card_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class BookCardComponent < ViewComponent::Base
+  def initialize(book:)
+    @book = book
+  end
+
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,16 @@ class Book < ApplicationRecord
 
   scope :in_old_testament, -> { where(position: ..39)}
   scope :in_new_testament, -> { where(position: 40..)}
+
+  def completed_chapter_count
+    chapters_data.count { |data| data["completed_at"].present? }
+  end
+
+  def completed_all_chapters?
+    completed_chapter_count == chapters_count
+  end
+
+  def completed_chapter_percentage
+    (completed_chapter_count.to_f/chapters_count * 100).ceil
+  end
 end
