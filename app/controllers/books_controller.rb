9b8f784bc34f695d853e229d430d2475a403ed04
Paul Kim <pkim@hey.com>
2024-04-20 21:51:35 -0400

    Move turbo stream file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,28 +4,24 @@ class BooksController < ApplicationController
   end
 
   def toggle_chapter
-    book = current_user.reading_log.books.find(params[:book_id])
-    chapter_number = params[:chapter_number].to_i
+    @book = current_user.reading_log.books.find(params[:book_id])
+    @chapter_number = params[:chapter_number].to_i
 
-    chapters_data = book.chapters_data
-    chapter_to_update = chapters_data.find { |chapter| chapter["chapter_number"] == chapter_number}
+    chapters_data = @book.chapters_data
+    @chapter_to_update = chapters_data.find { |chapter| chapter["chapter_number"] == @chapter_number}
 
-    if chapter_to_update
-      if chapter_to_update["completed_at"].present?
-        chapter_to_update["completed_at"] = nil
+    if @chapter_to_update
+      if @chapter_to_update["completed_at"].present?
+        @chapter_to_update["completed_at"] = nil
       else
-        chapter_to_update["completed_at"] = Time.zone.now
+        @chapter_to_update["completed_at"] = Time.zone.now
       end
     end
 
-    if book.update(chapters_data: chapters_data)
+    if @book.update(chapters_data: chapters_data)
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: [
-          turbo_stream.replace("book-square-#{book.id}-#{chapter_number}", partial: "books/book_chapter_square", locals: { book: book, chapter_data: chapter_to_update }),
-          turbo_stream.replace("book-chapter-details-#{book.id}", partial: "books/book_chapter_details", locals: { book: book }),
-        ]
-        }
-        format.html         { redirect_to root_path }
+        format.turbo_stream
+        format.html { redirect_to root_path }
       end
     else
       flash[:alert] = "Sorry, something went wrong."
