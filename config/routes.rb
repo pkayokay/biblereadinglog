a577d6b289f61cd278b9322e08f83b12078fd848
Paul Kim <pkim@hey.com>
2024-04-22 15:57:48 -0400

    Clean up book routes to use ID not book_id

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   resources :reading_logs, only: [:show] do
     resources :books, only: [:show] do
-      patch "toggle_chapter", to: "books#toggle_chapter"
+      member do
+        patch "toggle_chapter", to: "books#toggle_chapter"
+      end
     end
 
     member do
@@ -22,12 +24,6 @@ Rails.application.routes.draw do
 
   get "admin", to: "base#admin"
   post "setup_user", to: "base#setup_user"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
