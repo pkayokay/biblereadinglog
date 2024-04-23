5707c9428cd636a038198b3f4226875b117eab26
Paul Kim <pkim@hey.com>
2024-04-22 21:44:03 -0400

    Clean up password reset form

--- app/components/input_component.rb
+++ app/components/input_component.rb
@@ -2,14 +2,15 @@
 
 class InputComponent < ViewComponent::Base
   erb_template <<-ERB
-    <%= @form.send(field_type, @id_and_name, class: "form-input", required: @required) %>
+    <%= @form.send(field_type, @id_and_name, class: "form-input " + @custom_classes, required: @required) %>
   ERB
 
-  def initialize(form:, type:, id_and_name:, required: false)
+  def initialize(form:, type:, id_and_name:, required: false, custom_classes: "")
     @form = form
     @type = type
     @id_and_name = id_and_name
     @required = required
+    @custom_classes = custom_classes
   end
 
   def field_type
