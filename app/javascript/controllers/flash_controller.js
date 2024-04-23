e87d59ad4096ee0716ae1225c2632f86c0d58d68
Paul Kim <pkim@hey.com>
2024-04-23 16:22:50 -0400

    Improve flash message

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="flash"
+export default class extends Controller {
+  connect() {
+    requestAnimationFrame(() => {
+      this.element.classList.add("active");
+    });
+
+    setTimeout(() => this.close(), 5000);
+  }
+
+  close() {
+    this.element.classList.add("leaving");
+    this.element.classList.remove("active");
+    this.element.addEventListener("transitionend", () => {
+      this.element.remove();
+    });
+  }
+}
