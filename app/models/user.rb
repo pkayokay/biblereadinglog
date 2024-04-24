d583bec5008f184807d65edd5ee300c07fe1acdc
Paul Kim <pkim@hey.com>
2024-04-24 13:07:32 -0400

    Add column

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
   normalizes :email, with: ->(email) { email.strip.downcase }
   has_many :reading_logs, dependent: :destroy
 
+  enum color_theme: {
+    green: 0,
+    red: 1,
+    blue: 2,
+    gray: 3
+  }
+
   generates_token_for :password_reset, expires_in: 15.minutes do
     password_salt&.last(10)
   end
