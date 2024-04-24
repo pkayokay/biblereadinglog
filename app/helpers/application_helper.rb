6a37dc064a81f5e3c83f490ed0fb3ad42fda42ce
Paul Kim <pkim@hey.com>
2024-04-24 13:23:10 -0400

    Add dynamic themes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,27 @@
 module ApplicationHelper
+  def theme_values
+    if current_user.green?
+      {
+        line: "bg-green-500",
+        square: "shadow-green-100 border-green-300 bg-green-50 hover:bg-green-100",
+      }
+    elsif current_user.blue?
+      {
+        line: "bg-blue-500",
+        square: "shadow-blue-100 border-blue-300 bg-blue-50 hover:bg-blue-100",
+      }
+    elsif current_user.red?
+      {
+        line: "bg-red-500",
+        square: "shadow-red-100 border-red-300 bg-red-50 hover:bg-red-100",
+      }
+    elsif current_user.gray?
+      {
+        line: "bg-neutral-700",
+        square: "shadow-neutral-100 border-neutral-300 bg-neutral-100 hover:bg-neutral-200",
+      }
+    end
+  end
   def format_date(datetime_value)
     datetime_value.in_time_zone(current_user.time_zone).strftime('%A %_m/%-e at %l:%M %p')
   end
