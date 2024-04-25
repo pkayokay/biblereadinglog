3238601b83a598c27a55e05c704e74a87ca47d86
Paul Kim <pkim@hey.com>
2024-04-24 20:45:16 -0400

    Add name update form on settings page

--- app/controllers/reading_logs_controller.rb
+++ app/controllers/reading_logs_controller.rb
@@ -1,5 +1,5 @@
 class ReadingLogsController < ApplicationController
-  before_action :set_reading_log, only: [:show, :settings]
+  before_action :set_reading_log, only: [:update, :show, :settings]
 
   def index
     @reading_logs = current_user.reading_logs.order(created_at: :desc)
@@ -13,11 +13,20 @@ class ReadingLogsController < ApplicationController
     @reading_log = ReadingLog.new
   end
 
+  def update
+    if @reading_log.update(reading_log_params)
+      redirect_to reading_log_path(@reading_log), notice: "Reading log updated"
+    else
+      @errors = @reading_log.errors
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def create
     @reading_log = current_user.reading_logs.new(reading_log_params)
 
     if @reading_log.save
-      redirect_to reading_log_path(@reading_log)
+      redirect_to reading_log_path(@reading_log), notice: "Reading log created"
     else
       @errors = @reading_log.errors
       render :new, status: :unprocessable_entity
@@ -36,6 +45,9 @@ class ReadingLogsController < ApplicationController
     add_breadcrumb("Home", root_path)
     add_breadcrumb(@reading_log.name, reading_log_path(@reading_log))
     add_breadcrumb("Settings", settings_reading_log_path(@reading_log))
+
+
+
   end
 
   private
--- app/models/reading_log.rb
+++ app/models/reading_log.rb
@@ -1,6 +1,7 @@
 class ReadingLog < ApplicationRecord
   validates :name, presence: true
-  normalizes :name, with: ->(name) { name.strip.capitalize }
+  validates_length_of :name, maximum: 60
+  normalizes :name, with: ->(name) { name.strip }
   has_many :books, dependent: :destroy
   has_many :ordered_books, -> { order(position: :asc)}, dependent: :destroy, class_name: "Book"
   belongs_to :user
