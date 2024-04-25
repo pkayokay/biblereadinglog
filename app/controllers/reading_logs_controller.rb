0aff5420bb019b02c84f3a9f559dc455f846b7d9
Paul Kim <pkim@hey.com>
2024-04-24 20:33:19 -0400

    Add name to reading log

--- app/controllers/reading_logs_controller.rb
+++ app/controllers/reading_logs_controller.rb
@@ -2,7 +2,7 @@ class ReadingLogsController < ApplicationController
   before_action :set_reading_log, only: [:show, :settings]
 
   def index
-    @reading_logs = current_user.reading_logs
+    @reading_logs = current_user.reading_logs.order(created_at: :desc)
 
     unless current_user.reading_logs.exists?
       redirect_to new_reading_log_path
@@ -14,18 +14,19 @@ class ReadingLogsController < ApplicationController
   end
 
   def create
-    @reading_log = current_user.reading_logs.new
+    @reading_log = current_user.reading_logs.new(reading_log_params)
+
     if @reading_log.save
       redirect_to reading_log_path(@reading_log)
     else
-      @erros = @reading_log.errors
-      render :new, :unprocessable_entity
+      @errors = @reading_log.errors
+      render :new, status: :unprocessable_entity
     end
   end
 
   def show
     add_breadcrumb("Home", root_path)
-    add_breadcrumb("Reading Log", reading_log_path(@reading_log))
+    add_breadcrumb(@reading_log.name, reading_log_path(@reading_log))
 
     @old_testament_books = @reading_log.ordered_books.in_old_testament
     @new_testament_books = @reading_log.ordered_books.in_new_testament
@@ -33,7 +34,7 @@ class ReadingLogsController < ApplicationController
 
   def settings
     add_breadcrumb("Home", root_path)
-    add_breadcrumb("Reading Log", reading_log_path(@reading_log))
+    add_breadcrumb(@reading_log.name, reading_log_path(@reading_log))
     add_breadcrumb("Settings", settings_reading_log_path(@reading_log))
   end
 
@@ -41,5 +42,9 @@ class ReadingLogsController < ApplicationController
 
   def set_reading_log
     @reading_log = current_user.reading_logs.find(params[:id])
+  end
+
+  def reading_log_params
+    params.require(:reading_log).permit(:name)
   end
 end
--- app/models/reading_log.rb
+++ app/models/reading_log.rb
@@ -1,4 +1,6 @@
 class ReadingLog < ApplicationRecord
+  validates :name, presence: true
+  normalizes :name, with: ->(name) { name.strip.capitalize }
   has_many :books, dependent: :destroy
   has_many :ordered_books, -> { order(position: :asc)}, dependent: :destroy, class_name: "Book"
   belongs_to :user
