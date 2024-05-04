cf55806db202e1309a058d35ace4e6c4bf2ca430
Paul Kim <pkim@hey.com>
2024-05-04 07:54:25 -0400

    Update

--- app/components/side_form_wrapper_component.rb
+++ app/components/side_form_wrapper_component.rb
@@ -2,8 +2,8 @@
 
 class SideFormWrapperComponent < ViewComponent::Base
   erb_template <<-ERB
-    <div class="mt-6 border-b pb-8 mb-8 flex md:flex-row flex-col <%= @custom_class %>">
-      <div class="w-[240px] mb-3 pr-4">
+    <div class="mt-6 border-b pb-8 mb-8 flex flex-col <%= @custom_class %>">
+      <div class="mb-3">
         <h3 class="mb-1 text-xl font-medium"><%= @title %></h3>
         <p class="opacity-70 text-base"><%= @description %></p>
       </div>
