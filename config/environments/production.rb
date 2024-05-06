16608d52201dfd3f911f28b20a37c2676966fa62
Paul Kim <pkim@hey.com>
2024-05-05 22:03:42 -0400

    Expire session after a month - fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.session_store :cookie_store, expire_after: 10.minutes
+  config.session_store :cookie_store, expire_after: 1.month
 end
