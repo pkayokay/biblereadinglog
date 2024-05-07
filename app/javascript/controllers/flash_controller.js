af8e2dfcc1509bd3f293c2c1e11a7add64b40843
Paul Kim <pkim@hey.com>
2024-05-07 10:42:07 -0400

    Update flash_controller.js

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
       this.element.classList.add("active");
     });
 
-    setTimeout(() => this.close(), 6000);
+    setTimeout(() => this.close(), 5000);
   }
 
   close() {
