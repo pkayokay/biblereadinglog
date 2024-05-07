ec684abaf8ee224d785fdfd7eae55d7c76abbf7a
Paul Kim <pkim@hey.com>
2024-05-06 21:18:44 -0400

    Clean up emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,11 +5,11 @@ class UserMailer < ApplicationMailer
 
   def password_reset
     return if skip_email?
-    mail to: params[:user].email
+    mail to: params[:user].email, subject: "Reset Your Password"
   end
 
   def email_confirmation
     return if skip_email?
-    mail to: params[:user].email, subject: "Bible Reading Log - Confirm Your Email Address"
+    mail to: params[:user].email, subject: "Confirm Your Email Address for Bible Reading Log"
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def password_reset
-    UserMailer.with(user: User.first).password_reset
+    UserMailer.with(user: User.first, token: "12345").password_reset
   end
 
   def email_confirmation
