cf7b61769027bb59d0680af19912849f2b7a1496
Paul Kim <pkim@hey.com>
2024-05-06 21:47:21 -0400

    Update user_mailer_preview.rb

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,10 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def password_reset
-    UserMailer.with(user: User.first, token: "12345").password_reset
+    UserMailer.with(user: User.last, token: "12345").password_reset
   end
 
   def email_confirmation
-    UserMailer.with(user: User.first, token: "12345").email_confirmation
+    UserMailer.with(user: User.last, token: "12345").email_confirmation
   end
 end
