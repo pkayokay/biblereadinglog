8d31dfb9a661316b2d435ae3bbc2c8f210a4f8d8
Paul Kim <pkim@hey.com>
2024-05-12 20:32:02 -0400

    Add colors

--- app/javascript/controllers/book_selection_controller.js
+++ app/javascript/controllers/book_selection_controller.js
@@ -1,13 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
+const BORDER_COLOR = "border-neutral-300"
+const BG_COLOR = "bg-neutral-100"
 // Connects to data-controller="book-selection"
 export default class extends Controller {
-  static targets = ["bookList"];
+  static targets = ["bookList","trueOption", "falseOption"];
+
+  connect() {
+    this.trueOptionTarget.classList.add(BG_COLOR)
+    this.trueOptionTarget.classList.add(BORDER_COLOR)
+  }
 
   toggleRadio(e) {
     if (e.target.value === "true") {
+      this.falseOptionTarget.classList.remove(BG_COLOR)
+      this.trueOptionTarget.classList.add(BG_COLOR)
+      this.falseOptionTarget.classList.remove(BORDER_COLOR)
+      this.trueOptionTarget.classList.add(BORDER_COLOR)
       this.bookListTarget.classList.add("hidden")
     } else {
+      this.trueOptionTarget.classList.remove(BG_COLOR)
+      this.falseOptionTarget.classList.add(BG_COLOR)
+      this.trueOptionTarget.classList.remove(BORDER_COLOR)
+      this.falseOptionTarget.classList.add(BORDER_COLOR)
       this.bookListTarget.classList.remove("hidden")
     }
   }
