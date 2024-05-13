36bde6b3a0d0e39c72d76c977b7aa1ed36425457
Paul Kim <pkim@hey.com>
2024-05-13 10:23:50 -0400

    Add inter back

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -16,6 +18,9 @@ module.exports = {
         'heading': ['1.75rem','2.25rem'],
         'breadcrumbs': ['0.95rem'],
         'subheading': ['1.35rem','2.25rem']
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
