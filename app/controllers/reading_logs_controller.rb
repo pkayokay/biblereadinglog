26c084f482c5d170f744c4972895cf694806ce5b
Paul Kim <pkim@hey.com>
2024-05-14 10:19:03 -0400

    Update

--- app/controllers/reading_logs_controller.rb
+++ app/controllers/reading_logs_controller.rb
@@ -76,7 +76,7 @@ class ReadingLogsController < ApplicationController
     @skip_turbo_cache_control = true
     @back_button_values = {
       path: root_path,
-      text: "Back"
+      text: "Back to Reading Logs"
     }
 
     set_reading_log_show_breadcrumb(with_link: false)
