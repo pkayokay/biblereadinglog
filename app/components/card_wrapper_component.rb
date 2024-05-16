4fe3cb825177760ef467bb3e76fba1294ef5fa23
Paul Kim <pkim@hey.com>
2024-05-16 16:43:23 -0400

    Update cards to not be cards

--- app/components/card_wrapper_component.rb
+++ app/components/card_wrapper_component.rb
@@ -2,8 +2,12 @@
 
 class CardWrapperComponent < ViewComponent::Base
   erb_template <<-ERB
-    <div class="px-5 sm:px-8 <%= request.path == root_path ? 'pt-8' : 'py-8' %> flex-1 relative bg-white border-0 sm:border border-gray-200 sm:shadow-sm sm:rounded-lg mx-auto w-full max-w-2xl">
+    <div class="px-5 sm:px-8 <%= request.path == root_path ? 'pt-8' : 'py-8' %> <%= @custom_classes %> flex-1 relative bg-white border-0 sm:border border-gray-200 sm:shadow-sm sm:rounded-lg mx-auto w-full max-w-2xl">
       <%= content %>
     </div>
   ERB
+
+  def initialize(custom_classes: "")
+    @custom_classes = custom_classes
+  end
 end
