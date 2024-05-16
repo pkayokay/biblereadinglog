6cb68cf870b1c111054d492b10dfebccfe9cf416
Paul Kim <pkim@hey.com>
2024-05-15 21:40:13 -0400

    Update

--- app/components/back_button_component.rb
+++ app/components/back_button_component.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class BackButtonComponent < ViewComponent::Base
+  renders_one :right_content
+
   erb_template <<-ERB
-    <%= link_to @path, class: "inline-flex items-center space-x-1 text-base hover:underline " + (@include_margin ? 'mb-9' : '')  do %>
-      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
-      <span><%= @title %></span>
-    <% end %>
+    <div class="-mt-2 w-full flex items-center justify-between">
+      <%= link_to @path, class: "inline-flex items-center space-x-1 text-base hover:underline " + (@include_margin ? 'mb-9' : '')  do %>
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
+        <span><%= @title %></span>
+      <% end %>
+      <%= right_content %>
+    </div>
   ERB
 
   def initialize(title: "Back", path:, include_margin: false)
