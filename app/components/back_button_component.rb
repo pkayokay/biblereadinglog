72a49de4c5590ddf7a87695e938c036648fb28ce
Paul Kim <pkim@hey.com>
2024-05-16 15:33:58 -0400

    Update

--- app/components/back_button_component.rb
+++ app/components/back_button_component.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class BackButtonComponent < ViewComponent::Base
+  renders_one :middle_content
   renders_one :right_content
 
   erb_template <<-ERB
@@ -8,6 +9,11 @@ class BackButtonComponent < ViewComponent::Base
       <%= link_to @path, class: "inline-flex items-center space-x-1 text-base hover:underline"  do %>
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
         <span><%= @title %></span>
+      <% end %>
+      <% if middle_content %>
+        <div class="absolute text-center -translate-x-1/2 left-1/2 top-4">
+          <%= middle_content %>
+        </div>
       <% end %>
       <%= right_content %>
     </div>
