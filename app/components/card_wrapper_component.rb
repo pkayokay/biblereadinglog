95375352775b7f1a0ba7f573d591c4ad6b0bbe89
Paul Kim <pkim@hey.com>
2024-05-16 15:13:24 -0400

    Add card component

---
+++ app/components/card_wrapper_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class CardWrapperComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <div class="px-5 sm:px-8 <%= request.path == root_path ? 'pt-8' : 'py-8' %> flex-1 relative bg-white border-0 sm:border border-gray-200 md:shadow-sm md:rounded-lg mx-auto w-full max-w-2xl">
+      <%= content %>
+    </div>
+  ERB
+end
