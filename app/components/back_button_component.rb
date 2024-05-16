6e69836c01a4225f07a90a607b195f90e262c201
Paul Kim <pkim@hey.com>
2024-05-16 15:41:34 -0400

    Update

--- app/components/back_button_component.rb
+++ app/components/back_button_component.rb
@@ -5,7 +5,7 @@ class BackButtonComponent < ViewComponent::Base
   renders_one :right_content
 
   erb_template <<-ERB
-    <div class="mt-6  mb-6 mx-auto max-w-2xl w-full flex items-start justify-between px-4 sm:px-0">
+    <div class="mt-6  mb-8 mx-auto max-w-2xl w-full flex items-center justify-between px-4 sm:px-0">
       <%= link_to @path, class: "inline-flex items-center space-x-1 text-base hover:underline"  do %>
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
         <span><%= @title %></span>
