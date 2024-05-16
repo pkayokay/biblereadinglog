d81e7fed31f4fc2346e50dc6dcaa74921f38bbc3
Paul Kim <pkim@hey.com>
2024-05-16 15:24:01 -0400

    Update

--- app/components/back_button_component.rb
+++ app/components/back_button_component.rb
@@ -4,8 +4,8 @@ class BackButtonComponent < ViewComponent::Base
   renders_one :right_content
 
   erb_template <<-ERB
-    <div class="-mt-2 w-full flex items-center justify-between">
-      <%= link_to @path, class: "inline-flex items-center space-x-1 text-base hover:underline " + (@include_margin ? 'mb-9' : '')  do %>
+    <div class="mt-6  mb-6 mx-auto max-w-2xl w-full flex items-start justify-between px-4 sm:px-0">
+      <%= link_to @path, class: "inline-flex items-center space-x-1 text-base hover:underline"  do %>
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
         <span><%= @title %></span>
       <% end %>
--- app/components/card_wrapper_component.rb
+++ app/components/card_wrapper_component.rb
@@ -2,7 +2,7 @@
 
 class CardWrapperComponent < ViewComponent::Base
   erb_template <<-ERB
-    <div class="px-5 sm:px-8 <%= request.path == root_path ? 'pt-8' : 'py-8' %> flex-1 relative bg-white border-0 sm:border border-gray-200 md:shadow-sm md:rounded-lg mx-auto w-full max-w-2xl">
+    <div class="px-5 sm:px-8 <%= request.path == root_path ? 'pt-8' : 'py-8' %> flex-1 relative bg-white border-0 sm:border border-gray-200 sm:shadow-sm sm:rounded-lg mx-auto w-full max-w-2xl">
       <%= content %>
     </div>
   ERB
