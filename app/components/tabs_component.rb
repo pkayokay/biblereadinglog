449937952a9f737a1e8257235c21a5461fc4deb0
Paul Kim <pkim@hey.com>
2024-05-21 14:16:45 -0400

    Update

--- app/components/tabs_component.rb
+++ app/components/tabs_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class TabsComponent < ViewComponent::Base
-  def initialize(items: [], is_center: false)
+  def initialize(items: [], is_center: false, is_full_width: false)
     @items = items
     @is_center= is_center
+    @is_full_width = is_full_width
   end
 end
