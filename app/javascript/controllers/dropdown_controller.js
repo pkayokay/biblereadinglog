b7e8c54f68164a06e0d6443efe136a64b46a8a77
Paul Kim <pkim@hey.com>
2024-05-20 21:11:14 -0400

    Always hide

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,5 +3,8 @@ import { Dropdown } from "tailwindcss-stimulus-components";
 
 // Connects to data-controller="dropdown"
 export default class extends Dropdown {
-
+  hide(){
+    https://github.com/excid3/tailwindcss-stimulus-components/blob/main/src/dropdown.js
+    this.openValue = false
+  }
 }
