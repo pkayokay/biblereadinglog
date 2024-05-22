d35c31294cefb7f4a8b804b711b69c5a50c94e79
Paul Kim <pkim@hey.com>
2024-05-22 15:03:30 -0400

    Update

--- app/components/page_actions_header_component.rb
+++ app/components/page_actions_header_component.rb
@@ -4,7 +4,7 @@ class PageActionsHeaderComponent < ViewComponent::Base
   renders_one :right_content
 
   erb_template <<-ERB
-    <div class="mb-8 mx-auto flex items-center justify-between">
+    <div class="-mt-3 sm:mt-0 mb-8 mx-auto flex items-center justify-between">
       <%= link_to @path, class: "-ml-1 inline-flex items-center space-x-1 text-base opacity-40 font-medium hover:opacity-100"  do %>
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
         <span><%= @title %></span>
