012b48f02e66c5c785a292d156d9fe4dff2450e7
Paul Kim <pkim@hey.com>
2024-05-22 14:41:27 -0400

    Add invite button

--- app/components/page_actions_header_component.rb
+++ app/components/page_actions_header_component.rb
@@ -5,7 +5,7 @@ class PageActionsHeaderComponent < ViewComponent::Base
 
   erb_template <<-ERB
     <div class="mb-8 mx-auto flex items-center justify-between">
-      <%= link_to @path, class: "-ml-1 inline-flex items-center space-x-1 text-base hover:underline opacity-40 font-medium hover:opacity-100"  do %>
+      <%= link_to @path, class: "-ml-1 inline-flex items-center space-x-1 text-base opacity-40 font-medium hover:opacity-100"  do %>
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
         <span><%= @title %></span>
       <% end %>
