91f4369a4cd97382567d32a585bbeab862a2fa3b
Paul Kim <pkim@hey.com>
2024-05-21 20:15:01 -0400

    Rename back button to page header

--- app/components/page_header_component.rb
+++ app/components/page_header_component.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-class BackButtonComponent < ViewComponent::Base
-  renders_one :middle_content
+class PageHeaderComponent < ViewComponent::Base
   renders_one :right_content
 
   erb_template <<-ERB
@@ -10,17 +9,12 @@ class BackButtonComponent < ViewComponent::Base
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
         <span><%= @title %></span>
       <% end %>
-      <% if middle_content %>
-        <%= middle_content %>
-      <% end %>
       <%= right_content %>
     </div>
   ERB
 
-  def initialize(title: "Back", path:, include_margin: false)
+  def initialize(title: "Back", path:)
     @title = title
     @path = path
-    @include_margin = include_margin
   end
-
 end
