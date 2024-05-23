1e948e935c917c78841a946779aa4c41d0082a55
Paul Kim <pkim@hey.com>
2024-05-23 13:07:20 -0400

    Update

--- app/components/auth_form_container_component.rb
+++ app/components/auth_form_container_component.rb
@@ -4,7 +4,7 @@ class AuthFormContainerComponent < ViewComponent::Base
   renders_one :footer
 
   erb_template <<-ERB
-    <div class="w-full max-w-[410px] mx-auto">
+    <div class="w-full max-w-sm mx-auto">
       <%= content %>
       <% if footer %>
         <div class="text-center mt-4 flex flex-col space-y-3 items-center">
