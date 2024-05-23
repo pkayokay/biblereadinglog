5ba653e744c7d7a392aaafbed840f74f9e92142c
Paul Kim <pkim@hey.com>
2024-05-23 12:43:36 -0400

    Show name field for non-owners but show disclaimer

--- app/components/submit_button_component.rb
+++ app/components/submit_button_component.rb
@@ -2,13 +2,14 @@
 
 class SubmitButtonComponent < ViewComponent::Base
   erb_template <<-ERB
-    <%= @form.submit @title, data: @data, class: "btn btn--primary " + @custom_classes %>
+    <%= @form.submit @title, data: @data, disabled: @disabled, class: "btn btn--primary " + @custom_classes %>
   ERB
 
-  def initialize(title:, form:, data: {}, custom_classes: "")
+  def initialize(title:, form:, data: {}, custom_classes: "", disabled: false)
     @title = title
     @form = form
     @custom_classes = custom_classes
     @data = data
+    @disabled = disabled
   end
 end
