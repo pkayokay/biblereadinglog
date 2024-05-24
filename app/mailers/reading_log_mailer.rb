ce803cf6f09fff233268bc388ab597b981157e5b
Paul Kim <pkim@hey.com>
2024-05-24 14:23:59 -0400

    Update reading_log_mailer.rb

--- app/mailers/reading_log_mailer.rb
+++ app/mailers/reading_log_mailer.rb
@@ -1,5 +1,7 @@
 class ReadingLogMailer < ApplicationMailer
   def email_reminder
+    return if skip_email?
+
     @user = params[:user]
     @reading_log = params[:reading_log]
     current_time = Time.now.in_time_zone(@user.time_zone).strftime("%a, %b %d")
