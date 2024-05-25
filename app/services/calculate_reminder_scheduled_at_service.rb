6a666b6c4a4c5445a60d8ddce98101da0f1e16b6
Paul Kim <pkim@hey.com>
2024-05-24 20:18:55 -0400

    Revert "Get utc time for current time"
    This reverts commit 7cdf8a53290fe49bf1317fabf968d9a2be5fe693.

--- app/services/calculate_reminder_scheduled_at_service.rb
+++ app/services/calculate_reminder_scheduled_at_service.rb
@@ -80,7 +80,7 @@ class CalculateReminderScheduledAtService
   end
 
   def current_time
-    Time.current.in_time_zone(user.time_zone)
+    Time.now.in_time_zone(user.time_zone)
   end
 
   def remimder_time
