956935181aabd8f65cdaf8d6c780d562b18b1e80
Paul Kim <paul@paulkim.net>
2025-05-21 10:19:19 -0400

    Run solid queue in server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,6 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 end
